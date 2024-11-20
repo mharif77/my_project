826d08144a00cf8d38dd28c7d79f09265e2ac0e4
MD ARIF HASAN <mdarifhasan077@gmail.com>
2024-11-21 03:34:52 +0600

    Home work 20.11.24

--- resources/views/backend/admin_dashboard.blade.php
+++ resources/views/backend/admin_dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
 				
 				<!-- Title -->
-				<div class="row heading-bg  bg-yellow">
+				<div class="row heading-bg  bg-green">
 					<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
 						<h5 class="txt-light">demographical</h5>
 					</div>
